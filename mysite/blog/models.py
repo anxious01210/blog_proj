6d8c7daaf1b3a58e51d819f397a9c04c6796bf08
Mehdi <pinktheory21@gmail.com>
2020-07-26 18:01:19 +0300

    7th: Post class-based views and their correspond URLs

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     create_field = models.DateTimeField(default=timezone.now())
@@ -22,8 +22,8 @@ class Post(models.Model):
         return self.title
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', related_name='comments')
-    auther = models.CharField(max_length=200)
+    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
     approved_comment = models.BooleanField(default=False)
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required # function-based
+from django.contrib.auth.mixins import LoginRequiredMixin # class-based
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView,UpdateView,DeleteView)
+from blog.models import Post, Comment
+from blog.forms import PostForm, CommentForm
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    # define how to grab the list, it (def get_queryset(self):) allows you to use django's ORM when dealing
+    # with just generic views and ables you to add alittle bit costume touch to it.
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView): # Now you are inheriting methods from both classes
+    # With mixins your are going to need the 2 below necessary attributes
+    login_url = '/login/' # In case the user is not loged-in where the user should go? (here ==> '/login/')
+    redirect_field_name = 'blog/post_detail.html' # Just saying to where to redirect the visitors
+    form_class = PostForm
+    model = post
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = post
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('published_date')
